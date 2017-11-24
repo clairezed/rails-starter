94cc83292b91f8db52c2c43a0efba75b6852dd2e
clairezed <clairezuliani@gmail.com>
2017-11-24 14:52:55 +0100

    Try to debug travis not finding font awaesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w[administration.css administration.js]
-# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
